aa5f9a522786a155dfb4892a1caca6824e6e2115
ditimehta24 <124592087+ditimehta24@users.noreply.github.com>
2023-02-17 20:19:51 +0530

    Add files via upload

---
+++ Assignment 1-area.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 20:20:34 2023
+
+@author: Diti Mehta
+"""
+
+import math
+radius=float(input("Input the radius of the circle: "))
+area=math.pi*(radius**2)
+print("The area of the circle with radius",radius,"is: ",area)

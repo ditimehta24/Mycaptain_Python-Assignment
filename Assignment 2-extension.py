3c8450d318152dfee3fcf8ec19e2965b2326cf26
ditimehta24 <124592087+ditimehta24@users.noreply.github.com>
2023-02-27 19:13:12 +0530

    Add files via upload

---
+++ Assignment 2-extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 20:26:45 2023
+
+@author: Diti Mehta
+"""
+
+filename=input("Enter a filename: ")
+baseline,extension=filename.split(".")
+print("The extension of the file is:", extension)

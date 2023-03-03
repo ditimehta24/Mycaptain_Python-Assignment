b5c4f4b0ca293227e2a17f7a95151cb3ffca845c
ditimehta24 <124592087+ditimehta24@users.noreply.github.com>
2023-03-03 17:45:27 +0530

    Add files via upload

---
+++ Assignment 3-Fibonacci series.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 17:41:03 2023
+
+@author: Diti Mehta
+"""
+
+n= int(input("How many terms? "))
+n1,n2 = 0,1
+count=0
+if n==1:
+   print("Fibonacci sequence upto",n,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count<n:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count=count+1

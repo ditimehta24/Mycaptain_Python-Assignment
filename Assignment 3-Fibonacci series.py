1e1c83137b098a2064a87f03ca3891e444b87886
ditimehta24 <124592087+ditimehta24@users.noreply.github.com>
2023-03-03 17:56:15 +0530

    Update Assignment 3-Fibonacci series.py

--- Assignment 3-Fibonacci series.py
+++ Assignment 3-Fibonacci series.py
@@ -15,7 +15,7 @@ else:
    print("Fibonacci sequence:")
    while count<n:
        print(n1)
-       nth = n1 + n2
-       n1 = n2
-       n2 = nth
+       nth=n1+n2
+       n1=n2
+       n2=nth
        count=count+1
